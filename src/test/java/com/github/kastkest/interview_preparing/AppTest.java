414a76bf3fa2b1cba6ad1b7078a765c0e21a0c7f
kastkest <endimion-xi@yandex.ru>
2023-01-23 18:18:19 +0400

    init: init project

---
+++ src/test/java/com/github/kastkest/interview_preparing/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.kastkest.interview_preparing;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
