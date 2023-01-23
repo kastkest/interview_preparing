ef875e0f077658e00bde771e67cfaf40028fccf6
kastkest <endimion-xi@yandex.ru>
2023-01-23 18:49:16 +0400

    feat: review code

--- src/main/java/com/github/kastkest/interview_preparing/Main.java
+++ src/main/java/com/github/kastkest/interview_preparing/Main.java
@@ -1,5 +1,13 @@
 package com.github.kastkest.interview_preparing;
 
+import com.github.kastkest.interview_preparing.shapes.Circle;
+import com.github.kastkest.interview_preparing.shapes.Rectangle;
+import com.github.kastkest.interview_preparing.shapes.Shape;
+import com.github.kastkest.interview_preparing.shapes.Triangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Person person = Person.Builder.createBuilder()
@@ -8,5 +16,14 @@ public class Main {
                 .build();
 
         System.out.println(person);
+
+        List<Shape> shapes= new ArrayList<>();
+        shapes.add(new Circle());
+        shapes.add(new Rectangle());
+        shapes.add(new Triangle());
+
+        for (Shape shape : shapes) {
+            shape.rotate();
+        }
     }
 }
---
+++ src/main/java/com/github/kastkest/interview_preparing/shapes/Rectangle.java
@@ -0,0 +1,8 @@
+package com.github.kastkest.interview_preparing.shapes;
+
+public class Rectangle extends Shape {
+    @Override
+    public void rotate() {
+        System.out.println("Я же квадрат");
+    }
+}
